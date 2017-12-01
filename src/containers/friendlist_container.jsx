7b35a687255392767fd65181ba737f6e29f7aec8
Vestige717 <Vestige717@gmail.com>
2017-12-01 11:15:22 -0800

    added client side friend request logic

--- src/containers/friendlist_container.jsx
+++ src/containers/friendlist_container.jsx
@@ -5,18 +5,38 @@ import axios from 'axios'
 class Friendlist extends Component {
   constructor(props) {
     super(props)
+    this.handleFindFriend = this.handleFindFriend.bind(this);
     this.state = {
 
     }
   }
-
+  handleFindFriend(){
+    console.log(document.getElementById('friendSearchBar').value);
+    let newFriend = document.getElementById('friendSearchBar').value;
+    let currentUser = this.props.currentUserStore;
+    console.log(' this is the current logged user ',currentUser)
+    let friendRequest = {
+      requestee: currentUser,
+      requested: newFriend
+    }
+    axios.post('/addFriend', friendRequest)
+  }
 
   render() {
     return (
-      <div className="friendlist">
-        This is Friendlist
+      <div className="friendlist" id="friendsComponent">
+        <h2>find friend</h2>
+        <input id="friendSearchBar"></input>
+        <button id="findFriendButton" onClick={this.handleFindFriend}>add</button>
+        <div></div>
       </div>
     )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    currentUserStore: state.currentUserStore
   }
 }
 
