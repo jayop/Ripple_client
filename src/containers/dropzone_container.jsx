2a9d478169ba57a8520d4261a14053396e7e1e36
Vestige717 <Vestige717@gmail.com>
2017-12-08 09:57:25 -0800

    client side file transfer logic

--- src/containers/dropzone_container.jsx
+++ src/containers/dropzone_container.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { FormGroup } from 'react-bootstrap'
 import axios from 'axios'
+import io from 'socket.io-client'
+import SocketIOFileUpload from 'socketio-file-upload';
 
 class Dropzone extends Component {
   constructor(props) {
@@ -9,12 +11,16 @@ class Dropzone extends Component {
 
     }
   }
-
-
+  componentDidMount(){
+    var socket = io.connect();
+    var uploader = new SocketIOFileUpload(socket);
+    uploader.listenOnInput(document.getElementById("siofu_input"));
+  }
   render() {
     return (
       <div className="dropzone">
         <h1>DROPZONE</h1>
+        <input type="file" id="siofu_input"></input>
       </div>
     )
   }
